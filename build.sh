20150483c2bad881a1ae087a26ce55b9712bc817
Zheng-Bote <zheng.bote@googlemail.com>
2026-09-23 08:41:08 +0200

    v0.2.0

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ MITM_VERSION=$(git describe --tags)
 
 CGO_ENABLED=0 go build -ldflags="-s -w -X main.version=${MITM_VERSION}" -o ./bin/mitm-collector-employee-tmp-assignment-ora main.go
 
-cp bin/mitm-collector-employee-tmp-assignment-ora ../../bin/bin/.
+cp bin/mitm-collector-employee-tmp-assignment-ora ../../app/bin/.
